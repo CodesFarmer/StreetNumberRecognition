8fa3a5a19c8da2c53b3495ddbc9949497b774921
CodesFarmer <jhonjoe.c@gmail.com>
2017-08-23 07:31:38 +0800

    PrepareData

--- src/gen_pnet_data.py
+++ src/gen_pnet_data.py
@@ -9,18 +9,22 @@ file_path = os.path.join(data_dir, "validation.mat")
 #print(file_path)
 mat_name = scio.loadmat(file_path)
 imgnames = mat_name['imgname']
+imgbboxes = mat_name['bboxes']
 print(imgnames.dtype)
+
+img_id = 5;
 img_path = os.path.join(data_dir, "train", imgnames[0,1][0])
 print(img_path)
-imgbboxes = mat_name['bboxes']
 print(imgbboxes.dtype)
 
 bbox_index = 0;
 label_idnex = 4;
 x1_index = 0;
-print("label", imgbboxes[0,1][0][bbox_index][label_idnex][0][0])
-print("x1", imgbboxes[0,1][0][bbox_index][x1_index][0][0])
+print("label", imgbboxes[0,img_id][0][bbox_index][label_idnex][0][0])
+print("x1", imgbboxes[0,img_id][0][bbox_index][x1_index][0][0])
 
-
-#file_path = os.path.join(data_dir, "train_gt.mat")
-#mat_gt = h5py.File(file_path)
+for imname, imgt in zip(imgnames, imgbboxes):
+    print(1)
+    # imname = iminfo[0]
+    # imgt = iminfo[1]
+    # print(imname)
